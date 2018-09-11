e2aca1ccdaeba247a50df2ac2878ee5a91f3169c
James N. V. Cash <james.nvc@gmail.com>
2018-09-11 14:25:44 -0400

    Set up pack for release

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(http2_client).
 title('An HTTP/2 client').
-version('1.0.0').
+version('0.1.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
-%% home('https://github.com/jamesnvc/???').
-%% download('https://github.com/jamesnvc/???/releases/*.zip').
+home('https://gitlab.com/jamesnvc/prolog_http2_client').
+download('https://github.com/jamesnvc/http2_client/releases/*.zip').
 provides(http2_client).
 requires(list_util).
 requires(delay).
