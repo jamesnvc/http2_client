b18345661741eed8e654ad985b1b91f69298c7c1
James N. V. Cash <james.nvc@gmail.com>
2018-12-20 12:37:39 -0500

    Bump version to 1.0.0

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(http2_client).
 title('An HTTP/2 client').
-version('0.8.0').
+version('1.0.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://gitlab.com/jamesnvc/prolog_http2_client').
 download('https://github.com/jamesnvc/http2_client/releases/*.zip').
