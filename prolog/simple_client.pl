28511b9fa46134661f10ae37f04735c91be61626
James N. V. Cash <james.nvc@gmail.com>
2019-12-17 19:43:04 -0500

    Block until request is done, so we can set headers

--- prolog/simple_client.pl
+++ prolog/simple_client.pl
@@ -6,13 +6,12 @@
 :- use_module(library(unix), [pipe/2]).
 :- use_module(library(url), [parse_url/2]).
 
-simple_complete_cb(OutStream, RespHeaders, RespHeaders, Body) :-
-    debug(xxx, "complete ~w ~w", [RespHeaders, Body]),
+simple_complete_cb(ThreadId, OutStream, Headers, Body) :-
     format(OutStream, "~s", [Body]),
-    close(OutStream).
+    close(OutStream),
+    thread_send_message(ThreadId, finished(Headers)).
 
-close_cb(Ctx, _Data) :-
-    debug(xxx, "closed", []).
+close_cb(_Ctx, _Data) :- true.
 
 url_base_path(URL, Base, Path) :-
     parse_url(URL, [protocol(Proto), host(Host)|URLAttrs]),
@@ -33,25 +32,26 @@ url_base_path(URL, Base, Path) :-
 
 http2_simple_open(Ctx, URL, Read, Options) :-
     url_base_path(URL, BaseURL, Path),
-    debug(xxx, "opening ~w", [BaseURL]),
     http2_open(BaseURL, Ctx, [close_cb(simple_client:close_cb(Ctx))]),
     pipe(Read, Write),
 
     ( memberchk(method(Meth), Options) ; Meth = get ),
     ( memberchk(headers(RespHeaders), Options) ; RespHeaders = _ ),
     string_upper(Meth, Method),
-    debug(xxx, "Requesting ~w ~w", [Method, Path]),
+    thread_self(ThisId),
     http2_request(Ctx, [':method'-Method, ':path'-Path|Opts],
                   [],
-                  simple_complete_cb(Write, RespHeaders)).
+                  simple_complete_cb(ThisId, Write)),
+    thread_get_message(finished(RespHeaders)).
 
 test :-
     debug(xxx),
     http2_simple_open(Ctx,
                       'https://nghttp2.org/httpbin/ip',
                       Stream,
-                      []),
+                      [headers(Headers)]),
     read_string(Stream, _, Body),
     debug(xxx, "body ~w", [Body]),
+    debug(xxx, "response headers ~w", [Headers]),
     close(Stream),
     http2_close(Ctx).
